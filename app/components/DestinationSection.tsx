df6b0d851bce29c99da5a551649f9872a8d45814
ndizeyedavid <davidndizeye101@gmail.com>
2025-12-26 14:35:13 +0200

    upd: redefined destination cards functionality

--- app/components/DestinationSection.tsx
+++ app/components/DestinationSection.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import styles from "./DestinationSection.module.css";
 import Image from "next/image";
+import Link from "next/link";
 export default function DestinationSection() {
   useEffect(() => {
     const elements = Array.from(
@@ -30,33 +31,45 @@ export default function DestinationSection() {
   return (
     <section className={styles.destins} id="destins">
       <div className={styles["destins-container"]}>
-        <div className={`${styles["destination-card"]} ${styles["fade-in"]}`}>
+        <Link
+          href="/packages?filter=rwanda"
+          className={`${styles["destination-card"]} ${styles["fade-in"]}`}
+        >
           <Image width={500} height={500} src="/cro.webp" alt="Rwanda" />
           <div className={styles["glass-overlay"]}>
             <h3>Rwanda</h3>
           </div>
-        </div>
+        </Link>
 
-        <div className={`${styles["destination-card"]} ${styles["fade-in"]}`}>
+        <Link
+          href="/packages?filter=uganda"
+          className={`${styles["destination-card"]} ${styles["fade-in"]}`}
+        >
           <Image width={500} height={500} src="/gorila.webp" alt="Uganda" />
           <div className={styles["glass-overlay"]}>
             <h3>Uganda</h3>
           </div>
-        </div>
+        </Link>
 
-        <div className={`${styles["destination-card"]} ${styles["fade-in"]}`}>
+        <Link
+          href="/packages?filter=tanzania"
+          className={`${styles["destination-card"]} ${styles["fade-in"]}`}
+        >
           <Image width={500} height={500} src="/mountain.webp" alt="Tanzania" />
           <div className={styles["glass-overlay"]}>
             <h3>Tanzania</h3>
           </div>
-        </div>
+        </Link>
 
-        <div className={`${styles["destination-card"]} ${styles["fade-in"]}`}>
+        <Link
+          href="/packages?filter=kenya"
+          className={`${styles["destination-card"]} ${styles["fade-in"]}`}
+        >
           <Image width={500} height={500} src="/lion.webp" alt="Kenya" />
           <div className={styles["glass-overlay"]}>
             <h3>Kenya</h3>
           </div>
-        </div>
+        </Link>
       </div>
     </section>
   );
